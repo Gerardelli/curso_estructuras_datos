237ac3297b1aff189ea62c7cab5d8bdda5540930
Gerardelli <estivcap@gmail.com>
2024-03-20 14:29:23 -0600

    Se agregaron los ejemplos de colas en JAVa

---
+++ colas1/src/Main.java
@@ -0,0 +1,22 @@
+
+
+public class Main {
+    public static void main(String[] args) {
+        //
+        ColaPedidos cola = new ColaPedidos();
+
+        cola.agregarCola(1,"Julian");
+        cola.agregarCola(2,"Alice");
+        cola.agregarCola(3,"Perla");
+        cola.agregarCola(4,"Carlos");
+
+        cola.obtener();
+
+        cola.eliminar();
+
+        cola.obtener();
+        cola.obtenerSiguiente();
+
+
+    }
+}
