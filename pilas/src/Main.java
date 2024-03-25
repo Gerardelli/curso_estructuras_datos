c6488c956905684db95b0b9c0368ea9839370d57
Gerardelli <estivcap@gmail.com>
2024-03-25 17:09:52 -0600

    prueba de cambios metodo Main de pilas

--- pilas/src/Main.java
+++ pilas/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Programa que agrega libros a una pila");
+        Pila pila = new Pila();
 
+        pila.apilar(12345, "Gerardelli", "Introduccion a la programacion");
     }
 }
